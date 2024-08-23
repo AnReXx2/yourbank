f6d3eda9958e4eb9b6c305cab48991a74bdc5e6a
AnReXx <anrid040@gmail.com>
2024-08-23 20:13:56 +0400

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,37 +13,35 @@ import {
 
 export default function Home() {
   return (
-    <>
-      <main className={styles.main}>
-        <Image
-          className={styles.absDesign}
-          src={absDesign}
-          alt="Abstract-Design"
-        />
-        <main className={styles.content}>
-          <section className={styles.intro}>
-            <Intro />
-          </section>
-          <section className={styles.products}>
-            <Products />
-          </section>
-          <section className={styles.useCases}>
-            <UseCases />
-          </section>
-          <section className={styles.features}>
-            <Features />
-          </section>
-          <section className={styles.faq}>
-            <Faq />
-          </section>
-          <section className={styles.testimonials}>
-            <Testimonials />
-          </section>
-          <section className={styles.cta}>
-            <Cta />
-          </section>
-        </main>
+    <main className={styles.main}>
+      <Image
+        className={styles.absDesign}
+        src={absDesign}
+        alt="Abstract-Design"
+      />
+      <main className={styles.content}>
+        <section className={styles.intro}>
+          <Intro />
+        </section>
+        <section className={styles.products}>
+          <Products />
+        </section>
+        <section className={styles.useCases}>
+          <UseCases />
+        </section>
+        <section className={styles.features}>
+          <Features />
+        </section>
+        <section className={styles.faq}>
+          <Faq />
+        </section>
+        <section className={styles.testimonials}>
+          <Testimonials />
+        </section>
+        <section className={styles.cta}>
+          <Cta />
+        </section>
       </main>
-    </>
+    </main>
   );
 }
