e497e9533ddb2377879d09e7650d878488ae04f7
AnReXx <anrid040@gmail.com>
2024-08-26 01:33:54 +0400

    final

--- src/app/sign_up/page.tsx
+++ src/app/sign_up/page.tsx
@@ -1,10 +1,82 @@
+import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
+import googleIcon from "../../../public/assets/icons/google_icon.svg";
+import fbIcon from "../../../public/assets/icons/fb_icon.svg";
+import appleIcon from "../../../public/assets/icons/apple_icon.png";
+import { TextContainer, Button, Testimonials } from "@/components";
 
 const SignUp = () => {
   return (
-    <div>
-      <h1>SignUp</h1>
-      <p>Welcome to the SignUp page.</p>
+    <div className={styles.sign}>
+      <div className={styles.textCont}>
+        <TextContainer
+          span="Sign Up"
+          text="Join our community today! Create an account to unlock exclusive features and personalized experiences."
+        />
+      </div>
+      <form className={styles.form} action="">
+        <div>
+          <input
+            className={styles.input}
+            type="text"
+            name="text"
+            id="text"
+            placeholder="Enter First Name"
+          />
+          <input
+            className={styles.input}
+            type="text"
+            name="text"
+            id="text"
+            placeholder="Enter Last Name"
+          />
+        </div>
+        <div>
+          <input
+            className={styles.input}
+            type="email"
+            name="mail"
+            id="mail"
+            placeholder="Enter your Email"
+          />
+          <input
+            className={styles.input}
+            type="password"
+            name="pass"
+            id="pass"
+            placeholder="Enter your Password"
+          />
+        </div>
+      </form>
+      <Link className={styles.link} href={""}>
+        Forgot Password?
+      </Link>
+      <div className={styles.final}>
+        <div className={styles.btns}>
+          <Button className={styles.btn} title="Sign Up" />
+          <button className={styles.butt}>Login</button>
+        </div>
+        <div className={styles.socials}>
+          <div className={styles.socCont}>
+            <div className={styles.line}></div>
+            <p>Or Continue with</p>
+            <div className={styles.line}></div>
+          </div>
+          <div className={styles.socIcons}>
+            <Link className={styles.icon} href={""}>
+              <Image src={googleIcon} alt="google_icon" />
+            </Link>
+            <Link className={styles.icon} href={""}>
+              <Image src={fbIcon} alt="facebook_icon" />
+            </Link>
+            <Link className={styles.icon} href={""}>
+              <Image src={appleIcon} alt="apple_icon" />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <Testimonials />
     </div>
   );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,6 +9,15 @@ import { Button, BurgerMenuSlide } from "@/components";
 
 const Header: React.FC = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [active, setActive] = useState<number>(0);
+
+  const handleClick = (clicked: number) => {
+    setActive(clicked);
+  };
+
+  const handleResetActive = (reset: number) => {
+    setActive(reset);
+  };
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
@@ -28,38 +37,66 @@ const Header: React.FC = () => {
 
   return (
     <header className={styles.header}>
-      <Link href={"/"}>
+      <Link href={"/"} onClick={() => handleResetActive(1)}>
         <Image src={logo} alt="headerLogo" />
       </Link>
       <nav className={`${styles.menu} ${isMenuOpen ? styles.active : ""}`}>
         <ul>
           <li>
-            <Link className={styles.link} href={"/"}>
+            <Link
+              className={`${styles.link} ${
+                active === 1 ? styles.activeLink : ""
+              }`}
+              href={"/"}
+              onClick={() => handleClick(1)}
+            >
               Home
             </Link>
           </li>
           <li>
-            <Link className={styles.link} href={"/careers"}>
+            <Link
+              className={`${styles.link} ${
+                active === 2 ? styles.activeLink : ""
+              }`}
+              href={"/careers"}
+              onClick={() => handleClick(2)}
+            >
               Careers
             </Link>
           </li>
           <li>
-            <Link className={styles.link} href={"/about"}>
+            <Link
+              className={`${styles.link} ${
+                active === 3 ? styles.activeLink : ""
+              }`}
+              href={"/about"}
+              onClick={() => handleClick(3)}
+            >
               About
             </Link>
           </li>
           <li>
-            <Link className={styles.link} href={"/security"}>
+            <Link
+              className={`${styles.link} ${
+                active === 4 ? styles.activeLink : ""
+              }`}
+              href={"/security"}
+              onClick={() => handleClick(4)}
+            >
               Security
             </Link>
           </li>
         </ul>
       </nav>
       <div className={styles.sign}>
-        <Link className={styles.signUp} href={"/sign_up"}>
+        <Link
+          className={styles.signUp}
+          href={"/sign_up"}
+          onClick={() => handleResetActive(0)}
+        >
           Sign Up
         </Link>
-        <Link href={"/login"}>
+        <Link href={"/login"} onClick={() => handleResetActive(0)}>
           <Button title="Login" />
         </Link>
       </div>
